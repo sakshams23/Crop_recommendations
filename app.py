417a56a15ce479b7ea00ce74fda26e82884eb8cf
Saksham Soni <sonisaksham20208@gmail.com>
2025-04-21 01:51:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,36 @@
-import streamlit as st
-
 import pickle
 from PIL import Image
 
-LogReg_model=pickle.load(open('LogReg_model.pkl','rb'))
-DecisionTree_model=pickle.load(open('DecisionTree_model.pkl','rb'))
-NaiveBayes_model=pickle.load(open('NaiveBayes_model.pkl','rb'))
-RF_model=pickle.load(open('RF_model.pkl','rb'))
+LogReg_model=pickle.load(open('LogReg.pkl','rb'))
+DecisionTree_model=pickle.load(open('DecisionTree.pkl','rb'))
+NaiveBayes_model=pickle.load(open('NaiveBayes.pkl','rb'))
+RF_model=pickle.load(open('RF.pkl','rb'))
 
 def classify(answer):
-    return answer[0]+" is the best crop for cultivation here."
+    return answer[0]+" is the best crop for cultivation in this condition."
 
 
 def main():
-    st.title("(Crop Recommender)...")
-    image=Image.open('cc.jpg')
-    st.image(image)
+    st.title("(Crop Recommendation using ML and IOT)...")
     html_temp = """
-    <div style="background-color:teal; padding:10px">
-    <h2 style="color:white;text-align:center;">Find The Most Suitable Crop</h2>
+    <div style="background-color:teal; padding:20px">
+    <h2 style="color:white;text-align:center;">Best crop for cultivation</h2>
     </div>
     """
 
     st.markdown(html_temp, unsafe_allow_html=True)
-    activities=['Naive Bayes (The Best Model)','Logistic Regression','Decision Tree','Random Forest']
-    option=st.sidebar.selectbox("which model would you like to use?",activities)
+    activities=['Naive Bayes (Accuracy: 98.86%)','Logistic Regression (Accuracy: 90.68%)','Decision Tree (Accuracy: 90.68%)','Random Forest (Accuracy: 99.54%)']
+    option=st.sidebar.selectbox("Choose model?",activities)
     st.subheader(option)
-    sn=st.slider('NITROGEN (N)', 0.0, 150.0)
-    sp=st.slider('PHOSPHOROUS (P)', 0.0, 150.0)
-    pk=st.slider('POTASSIUM (K)', 0.0, 210.0)
+    sn=st.slider('NITROGEN (N)', 0.0, 200.0)
+    sp=st.slider('PHOSPHOROUS (P)', 0.0, 200.0)
+    pk=st.slider('POTASSIUM (K)', 0.0, 200.0)
     pt=st.slider('TEMPERATURE', 0.0, 50.0)
     phu=st.slider('HUMIDITY', 0.0, 100.0)
     pPh=st.slider('Ph', 0.0, 14.0)
     pr=st.slider('RAINFALL', 0.0, 300.0)
     inputs=[[sn,sp,pk,pt,phu,pPh,pr]]
-    if st.button('Classify'):
+    if st.button('Predict'):
         if option=='Logistic Regression':
             st.success(classify(LogReg_model.predict(inputs)))
         elif option=='Decision Tree':
